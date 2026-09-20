39f1812f0c522f21c23be6f6204bc5efcc400ec2
laylabreu <lmabreu565@gmail.com>
2026-09-19 22:23:37 -0400

    Enable dauly schedule for shortage ingestion

--- dags/shortage_dag.py
+++ dags/shortage_dag.py
@@ -6,6 +6,6 @@ from airflow import DAG
 from datetime import datetime
 
 
-with DAG(dag_id= 'shortage_dag', start_date=datetime(2026, 9, 18) , schedule = None) as dag:
+with DAG(dag_id= 'shortage_dag', start_date=datetime(2026, 9, 18) , schedule = '@daily', catchup=False) as dag:
     shortage_task = PythonOperator(task_id = 'load_shortages', python_callable = fetch_shortages)
 
