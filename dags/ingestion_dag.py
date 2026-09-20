1ca434a861b9b9aeea794dfe874ac33cd1df9c65
laylabreu <lmabreu565@gmail.com>
2026-09-20 12:50:33 -0400

    Added NADAC task to ingestion DAG

--- dags/ingestion_dag.py
+++ dags/ingestion_dag.py
@@ -7,7 +7,8 @@ from airflow import DAG
 from airflow.providers.standard.operators.python import PythonOperator
 
 from run_shortage_ingestion import fetch_shortages
+from run_nadac_ingestion import fetch_nadac
 
-with DAG(dag_id= 'shortage_dag', start_date=datetime(2026, 9, 18) , schedule = '@daily', catchup=False) as dag:
+with DAG(dag_id= 'ingestion_dags', start_date=datetime(2026, 9, 18) , schedule = '@daily', catchup=False) as dag:
     shortage_task = PythonOperator(task_id = 'load_shortages', python_callable = fetch_shortages)
-
+    nadac_task = PythonOperator(task_id = 'load_prices', python_callable = fetch_nadac)
